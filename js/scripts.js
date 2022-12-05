696d8fdd4c1c0c664f583a90f37ccfca03f1efbf
Pedro <pedrodesales7@gmail.com>
2022-12-05 20:06:06 -0300

    refatora o código e adiciona o arquivo env

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-const apiKey = ""
-//  colocar a key em dot env
-const apiCountryURL = "https://countryflagsapi.com/png";
+require('dotenv').config()
+
 const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
 const cityInput = document.querySelector("#city-input");
@@ -10,7 +9,6 @@ const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
-const countryElement = document.querySelector("#country");
 const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
@@ -24,13 +22,14 @@ const suggestionButtons = document.querySelectorAll("#suggestions button");
 
 const toggleLoader = () => {
     loader.classList.toggle("hide");
-  };
+};
 
 const getWeatherData = async(city) => {
     toggleLoader();
     
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
-
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}&lang=pt_br`;
+    
+    console.log(process.env);
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
 
@@ -67,7 +66,6 @@ const showWeatherData = async (city) => {
         "src",
         `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
     );
-    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
     humidityElement.innerText = `${data.main.humidity}%`;
     windElement.innerText = `${data.wind.speed}km/h`;
     
